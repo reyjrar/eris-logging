70440d082ccba56129d76a03ae3a5861f68ceae3
Brad Lhotsky <brad@divisionbyzero.net>
2019-10-14 17:15:48 +0200

    Fix iptables parser for log lines with reference parts

--- lib/eris/log/context/iptables.pm
+++ lib/eris/log/context/iptables.pm
@@ -40,6 +40,7 @@ sub sample_messages {
     my @msgs = split /\r?\n/, <<'EOF';
 Dec  4 00:41:19 janus kernel: iptables - ACTION=DROP IN=eth0 OUT= MAC=d4:3d:7e:f8:f4:57:cc:e1:7f:ac:7a:18:08:00 SRC=99.46.177.250 DST=148.251.14.68 LEN=40 TOS=0x02 PREC=0x00 TTL=50 ID=0 DF PROTO=TCP SPT=19219 DPT=993 WINDOW=0 RES=0x00 RST URGP=0
 Dec  4 00:41:22 janus kernel: iptables - ACTION=outbound IN= OUT=eth0 SRC=148.251.14.68 DST=194.146.106.106 LEN=60 TOS=0x00 PREC=0x00 TTL=64 ID=59545 DF PROTO=TCP SPT=43210 DPT=53 WINDOW=14600 RES=0x00 SYN URGP=0
+Dec  4 00:41:22 janus kernel: iptables - ACTION=outbound IN= OUT=eth0 SRC=148.251.14.68 DST=74.54.97.59 LEN=68 TOS=0x00 PREC=0xC0 TTL=64 ID=51558 PROTO=ICMP TYPE=3 CODE=10 [SRC=74.54.97.59 DST=148.251.14.68 LEN=40 TOS=0x00 PREC=0x00 TTL=58 ID=13769 DF PROTO=TCP SPT=49602 DPT=993 WINDOW=29200 RES=0x00 SYN URGP=0 ]
 EOF
     return @msgs;
 }
@@ -75,7 +76,8 @@ const my %map => qw(
 sub contextualize_message {
     my ($self,$log) = @_;
 
-    my $content = $log->context->{message};
+    my ($content,$reference) = split /\[/, $log->context->{message};
+    $reference =~ s/\s*\]$// if $reference;
     my %data = ();
 
     foreach my $token (split /\s+/, $content) {
